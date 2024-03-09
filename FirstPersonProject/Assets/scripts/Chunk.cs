4a89b9e1da2e5f90e004ce0e83eaf201a7dfe372
PowerPants556 <146521497+PowerPants556@users.noreply.github.com>
2024-03-09 12:33:15 +0200

    RenderDistance

--- FirstPersonProject/Assets/scripts/Chunk.cs
+++ FirstPersonProject/Assets/scripts/Chunk.cs
@@ -8,15 +8,33 @@ public class Chunk : MonoBehaviour
     private Transform playerT;
     private bool isVisible;
     private Vector3 position;
-    void Start()
+    private void Start()
     {
         playerT = GameObject.Find("Player").transform;
         position = transform.position;
         isVisible = true;
     }
 
-    void Update()
+    private void Update()
     {
-        
+        float distance = Vector3.Distance(position, new Vector3 
+            (playerT.position.x, 0f, playerT.position.z));
+        if (distance > visibilitiDistance && isVisible)
+        {
+            SetActivity(false);
+        }
+        else if (distance < visibilitiDistance && !isVisible)
+        {
+            SetActivity(true);
+        }
+    }
+    private void SetActivity(bool isActive)
+    {
+        int childrenCount = transform.childCount;
+        for(int i = 0; i < childrenCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(isActive);
+        }
+        isVisible = isActive;
     }
 }
---
+++ FirstPersonProject/Assets/scripts/Enams.cs
@@ -0,0 +1,16 @@
+public enums BlockTypes
+{
+    GRASS = 70,
+    GROUND = 100
+}
+public enums ToolTypes
+{
+    PICKAXE = 1,
+    SWORD = 4
+}
+public enums ToolMaterials
+{
+    STONE = 1,
+    IRON = 2,
+    DIAMOND = 3
+}
